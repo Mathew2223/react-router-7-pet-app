da937c5a497fdef19824bc8703c7e8ef76477fc5
Ситниковы <sitnikovmatvey2008@gmail.com>
2026-04-09 10:28:59 +0300

    add search params func

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import './App.css';
-import { Routes, Route, Outlet, Link, NavLink, type NavLinkRenderProps, useParams } from 'react-router';
+import React, { useState } from 'react';
+import { 
+    Routes,
+    Route,
+    Outlet,
+    Link,
+    NavLink,
+    type NavLinkRenderProps,
+    useParams,
+    useNavigate,
+    useSearchParams
+} from 'react-router';
 
 const App = () => {
-    const users = [
+    const navigate = useNavigate();
+
+    const [users, setUsers] = useState([
         { id: '1', fullName: 'Robin Wieruch' },
         { id: '2', fullName: 'Sarah Finnley' },
-    ]
+    ]);
+
+    const handleRemoveUser = (userId: string | undefined) => {
+        setUsers((state) => state.filter((user) => user.id !== userId));
+
+        navigate('/users');
+    }
 
     return (
         <>
@@ -16,9 +35,9 @@ const App = () => {
             <Routes>
                 <Route element={<Layout />}>
                     <Route index path='home' element={<Home />} />
-                    <Route path='users' element={<Users users={users} />}>
-                        <Route path=':userId' element={<User />} />
-                    </Route>
+                        <Route path='users' element={<Users users={users} />}>
+                            <Route path=':userId' element={<User onRemoveUser={handleRemoveUser} />} />
+                        </Route>
                     <Route path='*' element={<NoMatch />} />
                 </Route>
             </Routes>
@@ -50,30 +69,64 @@ type User = {
     fullName: string;
 };
 
+type UserProps = {
+    onRemoveUser: (userId: string | undefined) => void;
+}
+
 type UsersProps = {
     users: User[];
 };
 
-const User = () => {
+const User = ({ onRemoveUser }) => {
     const { userId } = useParams();
 
     return (
         <>
             <h2>User: {userId}</h2>
+
+            <button type='button' onClick={() => onRemoveUser(userId)}>
+                Remove
+            </button>
+
             <Link to="/users">Back to Users</Link>
         </>
     )
 }
 
 const Users = ({ users }: UsersProps) => {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const searchTerm = searchParams.get('name') || '';
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.value;
+
+        if (name) {
+            setSearchParams({ name: event.target.value });
+        } else {
+            setSearchParams();
+        }
+    };
+
     return (
         <>
             <h2>Users</h2>
 
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearch}
+            />
+
             <ul>
-                {users.map((user) => (
+                {users.filter((user) => 
+                    user.fullName
+                        .toLowerCase()
+                        .includes(searchTerm.toLocaleLowerCase())
+                )
+                .map((user) => (
                     <li key={user.id}>
-                        <Link to={`/users/${user.fullName}`}>{user.fullName}</Link>
+                        <Link to={user.id}>{user.fullName}</Link>
                     </li>
                 ))}
             </ul>
